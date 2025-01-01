f1125f31cad0cf012ea1e7795563e722d9875bed
Ilya fedyaevv <fedaevi52@gmail.com>
2025-01-01 02:12:09 +0100

    Add entry points for the client (App.js, index.js)

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import Footer from "./componets/Home/Footer/Footer";
+import Navbar from "./componets/Home/Navbar/Navbar";
+import HomePage from "./componets/Home/HomePage/HomePage";
+import Login from "./componets/Authentication/Login/Login";
+import Register from "./componets/Authentication/Register/Register";
+import ProtectedRoute from "./componets/ProtectedRoute";
+import ProfilePage from "./componets/Home/Profile/ProfilePage";
+import GoalsPage from "./componets/Home/Goals/GoalsPage";
+
+const AppContent = () => {
+  const location = useLocation();
+
+  const hideFooterRoutes = ["/login", "/register"];
+
+  return (
+    <>
+      {!hideFooterRoutes.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/goals"
+          element={
+            <ProtectedRoute>
+              <GoalsPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      {!hideFooterRoutes.includes(location.pathname) && <Footer />}
+    </>
+  );
+};
+
+const App = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
+
+export default App;
