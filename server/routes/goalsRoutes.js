bf521a4d7de28e983b64ba52db94ae1d5a39e320
Ilya fedyaevv <fedaevi52@gmail.com>
2025-01-04 22:26:14 +0100

    for addBalance

--- server/routes/goalsRoutes.js
+++ server/routes/goalsRoutes.js
@@ -5,6 +5,7 @@ const {
   addGoal,
   updateGoal,
   deleteGoal,
+  addBalanceToGoal,
 } = require("../controllers/goalsController");
 const authenticateToken = require("../middleware/authenticateToken");
 
@@ -12,5 +13,6 @@ router.get("/", authenticateToken, getGoals);
 router.post("/", authenticateToken, addGoal);
 router.put("/:id", authenticateToken, updateGoal);
 router.delete("/:id", authenticateToken, deleteGoal);
+router.post("/:id/add-balance", authenticateToken, addBalanceToGoal);
 
 module.exports = router;
