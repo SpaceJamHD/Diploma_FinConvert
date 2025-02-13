849dcd552c5b1e08dae54dac46daa12c43a763cc
Ilya fedyaevv <fedaevi52@gmail.com>
2025-02-13 13:27:05 +0100

    Updated routes for transaction operations

--- server/routes/transactionsRoutes.js
+++ server/routes/transactionsRoutes.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../models/userModel");
 const {
+  getUserTransactions,
   getTransactionsByGoalId,
+  createTransaction,
 } = require("../controllers/transactionsController");
 const authenticateToken = require("../middleware/authenticateToken");
 
+router.get("/", authenticateToken, getUserTransactions);
+
 router.get("/:goalId", authenticateToken, getTransactionsByGoalId);
+
+router.post("/", authenticateToken, createTransaction);
+
+router.delete("/", authenticateToken, async (req, res) => {
+  const userId = req.user.id;
+  try {
+    await pool.query("DELETE FROM currency_transactions WHERE user_id = $1", [
+      userId,
+    ]);
+    res.json({ message: "История транзакций очищена" });
+  } catch (error) {
+    console.error("Ошибка при очистке истории:", error);
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+});
 
 module.exports = router;
